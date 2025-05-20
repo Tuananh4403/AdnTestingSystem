4daf1c6678292a696199bee0c418dbe2274dbfc0
Tuananh4403 <tuananh4403@gmail.com>
2025-05-20 23:53:41 +0700

    Add project files.

---
+++ AdnTestingSystem.FeedbackService.API/Controllers/FeedbacksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdnTestingSystem.FeedbackService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbacksController : ControllerBase
+    {
+    }
+}
---
+++ AdnTestingSystem.IdentityService.API/Controllers/RolesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdnTestingSystem.IdentityService.API.Controllers
+{
+    public class RolesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
